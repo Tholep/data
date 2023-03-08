1f7059b88ca3b00c39477a7aedcf8cda2a7cf1da
Tho Le <tho.le3839@gmail.com>
2023-03-08 11:27:45 +0100

    Add files via upload

---
+++ client_pipe.ps1
@@ -0,0 +1,41 @@
+﻿# Define the named pipe name
+$pipeName = "TestPipe"
+
+$serverName = "commando"
+# Create a named pipe client
+$pipe = New-Object System.IO.Pipes.NamedPipeClientStream($serverName, $pipeName, [System.IO.Pipes.PipeDirection]::InOut)
+
+# Connect to the named pipe server
+$pipe.Connect()
+
+# Create a StreamReader and StreamWriter for the named pipe
+$reader = New-Object System.IO.StreamReader($pipe)
+$writer = New-Object System.IO.StreamWriter($pipe)
+
+# Loop to read input from the user and send messages to the server
+while($true) {
+    # Prompt the user for a message to send to the server
+    $message = Read-Host "Enter message to send to server"
+
+    # Send the message to the server
+    $writer.WriteLine($message)
+    $writer.Flush()
+
+    # Read the response from the server
+    $response = $reader.ReadLine()
+
+    
+    # Check if the server has disconnected
+    if($message -eq "kill") {
+        break
+    }
+
+    # Display the response from the server
+    Write-Host "Client received response: $response"
+
+}
+
+# Clean up resources
+$reader.Close()
+$writer.Close()
+$pipe.Dispose()
---
+++ server_pipe.ps1
@@ -0,0 +1,47 @@
+﻿# Define the named pipe name
+$pipeName = "TestPipe"
+
+
+
+# Create a named pipe server
+$pipe = New-Object System.IO.Pipes.NamedPipeServerStream($pipeName, [System.IO.Pipes.PipeDirection]::InOut)
+
+
+Write-Host "Server is waiting for connection..."
+
+# Start listening for client connections
+$pipe.WaitForConnection()
+
+
+# Create a StreamReader and StreamWriter for the named pipe
+$reader = New-Object System.IO.StreamReader($pipe)
+$writer = New-Object System.IO.StreamWriter($pipe)
+
+# Loop to read messages from the client and respond
+while($true) {
+    # Read a message from the client
+    $message = $reader.ReadLine()
+
+
+    # Check if the client has disconnected
+    if($message -eq "kill") {
+        Write-Host "Server closed"
+        break
+    }
+
+    if ($message) {
+        # Display the message received from the client
+        Write-Host "Server received message: $message"
+        # Prompt the server user for a response message
+        $response = Read-Host "Enter response to client message"
+
+        # Send the response message to the client
+        $writer.WriteLine($response)
+        $writer.Flush()
+    }
+}
+
+# Clean up resources
+$reader.Close()
+$writer.Close()
+$pipe.Dispose()
